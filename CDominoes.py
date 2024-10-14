ad62a88e5306475750b00e6899d15a9a5d176f58
maximus_baconus <bt7021rh@go.minnstate.edu>
2024-10-14 16:07:26 -0500

    Adding Python Files

--- CDominoes.py
+++ CDominoes.py
@@ -1,5 +1,20 @@
+class dominoe():
+    def __init__(self, front, back):
+        self.front = front
+        self.back = back
+    def __repr__(self):
+        print(f"[{self.front}|{self.back}]")
+
 
 #Class CDominoes to contain the data structure with pieces;
 class CDominoes():
     def __init__(self):
-        pass
+        self.boneyard = []
+        self.board = []
+
+    def create_dominoes(self): #
+        for x in range(1,6):
+            for y in range(x, 6):
+                new_dominoe = dominoe(x, y)
+                self.boneyard.append(new_dominoe)
+
--- CTable.py
+++ CTable.py
@@ -1,5 +1,9 @@
 
+
 #Class CTable to show/display the sorted pieces;
 class CTable():
     def __init__(self):
+        pass
+
+    def display(self, board):
         pass
